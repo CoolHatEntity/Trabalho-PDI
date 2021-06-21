82dfcf2ae4f7125d52270e9c0c8dbfa8e3ea5be1
CoolHatEntity <matheus17menezes@gmail.com>
2021-06-21 11:43:23 -0300

    Adicionei o programa visual

--- script.m
+++ script.m
@@ -1,3 +1,28 @@
-imwrite(ans(:,:,1), "red.png");
-imwrite(ans(:,:,2), "green.png");
-imwrite(ans(:,:,3), "blue.png");
+
+clc; clear; close all;
+file = uigetfile;
+img = imread(file);
+img = rgb2hsv(img);
+img = img(:,:,2);
+[I, param]= imcrop(imcomplement(img));
+close all;
+disp(param);
+fig = uifigure;
+cg = uiaxes(fig, 'Position', [50, 70, 500, 300]);
+imshow(I, 'parent', cg);
+sld = uislider(fig, 'Position', [100 50 300 50], 'Value', 1, 'Limits', [0,1],...
+'ValueChangedFcn', @(sld, event)changeSensitivityFcn(event, cg, I));
+sld1 = uislider(fig, 'Position', [150 50 300 50], 'Value', 1, 'Limits', [0,1000],...
+'ValueChangedFcn', @(sld, event)changePixFcn(event, cg, I));
+function changeSensitivityFcn(event, cg, I)
+    sens = event.Value;
+    %sens = ceil(sens);
+    cg.Children.CData = im2uint8(bwareaopen((imbinarize(I, sens)),sens2)); 
+    drawnow();
+end
+function changePixFcn(event, cg, I)
+    sens2 = event.Value;
+    sens = ceil(sens);
+    cg.Children.CData = im2uint8( bwareaopen((imbinarize(I, sens)),sens2) ); 
+    drawnow();
+end
